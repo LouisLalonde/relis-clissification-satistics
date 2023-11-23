a365d29d5199b7d655565075c7a7e2db5872ee5b
LouisLalonde <louis.lalonde@umontreal.ca>
2023-11-22 20:31:45 -0500

    Added business logic to display plots based on the user choices. Added new types to modelize the domain

--- statistics/python/relis_statistics_lib.py
+++ statistics/python/relis_statistics_lib.py
@@ -5,6 +5,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from enum import Enum
+from typing import Type
 from matplotlib import ticker
 from FisherExact import fisher_exact
 from statsmodels.robust.scale import mad
@@ -46,6 +47,19 @@ class ContinuousVariables(Enum):
     publication_year = Variable("publication_year", "Publication year", FieldClassificationType.CONTINUOUS, False)
     targeted_year = Variable("targeted_year", "Targeted year", FieldClassificationType.CONTINUOUS, False)
 
+class DataFrame:
+    def __init__(self, data: pd.DataFrame, variable_type: Type[NominalVariables] | Type[ContinuousVariables]):
+        self.data = data
+        self.variable_type = variable_type
+
+class NominalDataFrame(DataFrame):
+    def __init__(self, data: pd.DataFrame, variable_type: Type[NominalVariables]):
+        super().__init__(data, variable_type)
+
+class ContinuousDataFrame(DataFrame):
+    def __init__(self, data: pd.DataFrame, variable_type: Type[ContinuousVariables]):
+        super().__init__(data, variable_type)
+
 ### Shared
 
 def removeEmptyStrings(df: pd.DataFrame):
@@ -59,6 +73,12 @@ def process_splitted_values(values: pd.Series, multiple: bool):
         return values.apply(lambda x: [item.strip() for item in re.split(rf'\{Multivalue.SEPARATOR.value}', x)])
 
     return values.apply(lambda x: [x])
+
+def display_data(dataFrame: pd.DataFrame, bool: bool):
+    if bool: print(dataFrame)
+
+def display_figure(plt, bool: bool):
+    if bool: plt.show()
 
 ### Data
 
@@ -74,12 +94,15 @@ def filter_row_by_field_type(paper, field_type):
     pd_row = {key: value["value"] for key, value in paper.items() if value['type'] == field_type}
     return pd_row
 
-nominal_df = pd.DataFrame([filter_row_by_field_type(paper, FieldClassificationType.NOMINAL.value) for paper in classification_data])
-continuous_df = pd.DataFrame([filter_row_by_field_type(paper, FieldClassificationType.CONTINUOUS.value) for paper in classification_data])
+nominal_data = pd.DataFrame([filter_row_by_field_type(paper, FieldClassificationType.NOMINAL.value) for paper in classification_data])
+continuous_data = pd.DataFrame([filter_row_by_field_type(paper, FieldClassificationType.CONTINUOUS.value) for paper in classification_data])
+
+nominal = NominalDataFrame(nominal_data, NominalVariables)
+continuous = ContinuousDataFrame(continuous_data, ContinuousVariables)
 
 if (Policies.DROPNA.value):
-    removeEmptyStrings(nominal_df)
-    removeEmptyStrings(continuous_df)
+    removeEmptyStrings(nominal.data)
+    removeEmptyStrings(continuous.data)
 
 ### DESCRIPTIVE STATS
 
@@ -104,8 +127,8 @@ def beautify_data_desc(field_name: str, data: pd.DataFrame):
 
 ## Frequency tables
 
-desc_distr_vector = {NominalVariables[field_name]: beautify_data_desc(field_name, nominal_df)
-                      for field_name in nominal_df.columns}
+desc_distr_vector = {NominalVariables[field_name]: beautify_data_desc(field_name, nominal.data)
+                      for field_name in nominal.data.columns}
 
 ## Bar plots
 
@@ -119,22 +142,22 @@ def generate_bar_plot(field_name: str, data: pd.DataFrame):
     sns.set_theme(style="whitegrid")
 
     # Create the plot
-    plt.figure(figsize=(10, 6))
-    barplot = sns.barplot(data=df, x="value", y="percentage", hue="n")
+    fig, ax = plt.subplots(figsize=(10, 6))
+    sns.barplot(data=df, x="value", y="percentage", hue="n")
 
     # Get metadata
     variable = get_variable(field_name, NominalVariables)
 
     # Set labels and title
     title = f"{variable.title} ~ Bar plot"
-    barplot.set_title(title)
-    barplot.set_xlabel(variable.title)
-    barplot.set_ylabel("Percentage")
+    plt.title(title)
+    plt.xlabel(variable.title)
+    plt.ylabel("Percentage")
 
-    return barplot.figure
+    return fig
 
-bar_plot_vector = {NominalVariables[field_name]: generate_bar_plot(field_name, nominal_df)
-                    for field_name in nominal_df.columns}
+bar_plot_vector = {NominalVariables[field_name]: generate_bar_plot(field_name, nominal.data)
+                    for field_name in nominal.data.columns}
 
 ## Statistics
 
@@ -164,8 +187,8 @@ def generate_statistics(field_name: str, data: pd.DataFrame):
     }
     return results
 
-statistics_vector = {ContinuousVariables[field_name]: generate_statistics(field_name, continuous_df)
-                      for field_name in continuous_df.columns}
+statistics_vector = {ContinuousVariables[field_name]: generate_statistics(field_name, continuous.data)
+                      for field_name in continuous.data.columns}
 
 ## Box Plots
 
@@ -175,7 +198,7 @@ def generate_box_plot(field_name: str, data: pd.DataFrame):
     variable = get_variable(field_name, ContinuousVariables)
 
     # Create the box plot
-    plt.figure(figsize=(10, 6))
+    fig, ax = plt.subplots(figsize=(10, 6))
     sns.boxplot(data=series, color='lightblue')
 
     # Overlay the mean point
@@ -190,10 +213,10 @@ def generate_box_plot(field_name: str, data: pd.DataFrame):
 
     plt.gca().yaxis.set_major_formatter(ticker.FormatStrFormatter('%0.0f'))
 
-    return plt
+    return fig
 
-box_plot_vector = {ContinuousVariables[field_name]: generate_box_plot(field_name, continuous_df)
-                    for field_name in continuous_df.columns}
+box_plot_vector = {ContinuousVariables[field_name]: generate_box_plot(field_name, continuous.data)
+                    for field_name in continuous.data.columns}
 
 ## Violin Plots
 
@@ -202,7 +225,7 @@ def generate_violin_plot(field_name: str, data: pd.DataFrame):
     
     variable = get_variable(field_name, ContinuousVariables)
 
-    plt.figure(figsize=(10, 6))
+    fig, ax = plt.subplots(figsize=(10, 6))
     sns.violinplot(data=series, color="lightgray")
 
     plt.title(f"{variable.title} ~ Violin plot")
@@ -210,10 +233,10 @@ def generate_violin_plot(field_name: str, data: pd.DataFrame):
     plt.xlabel("Density")
     plt.xticks([])
 
-    return plt
+    return fig
 
-violin_plot_vector = {ContinuousVariables[field_name]: generate_violin_plot(field_name, continuous_df)
-                       for field_name in continuous_df.columns}
+violin_plot_vector = {ContinuousVariables[field_name]: generate_violin_plot(field_name, continuous.data)
+                       for field_name in continuous.data.columns}
 
 ### EVOLUTIVE STATS
 
@@ -252,8 +275,8 @@ def expand_data(field_name: str, publication_year: pd.Series, data: pd.DataFrame
 
     return subset_data 
 
-violin_plot_vector = {NominalVariables[field_name]: expand_data(field_name, continuous_df["publication_year"], nominal_df)
-                       for field_name in nominal_df.columns}
+evo_distr_vector = {NominalVariables[field_name]: expand_data(field_name, continuous.data["publication_year"], nominal.data)
+                       for field_name in nominal.data.columns}
 
 ## Evolution Plots
 
@@ -263,7 +286,7 @@ def generate_evo_plot(field_name: str, publication_year: pd.Series, data: pd.Dat
     subset_data = beautify_data_evo(field_name, publication_year, variable, data)
 
     # Create a plot
-    plt.figure(figsize=(10, 6))
+    fig, ax = plt.subplots(figsize=(10, 6))
     sns.lineplot(data=subset_data, x='Year', y='Frequency', hue='Value', style='Value', markers=True)
 
     # Setting title, labels, and theme
@@ -271,11 +294,10 @@ def generate_evo_plot(field_name: str, publication_year: pd.Series, data: pd.Dat
     plt.xlabel('Year')
     plt.ylabel('Frequency')
     plt.grid(True)
-    return plt
+    return fig
 
-evolution_plot_vector = {NominalVariables[field_name]: generate_evo_plot(field_name, continuous_df["publication_year"], nominal_df)
-                          for field_name in nominal_df.columns}
-
+evolution_plot_vector = {NominalVariables[field_name]: generate_evo_plot(field_name, continuous.data["publication_year"], nominal.data)
+                          for field_name in nominal.data.columns}
 
 ### COMPARATIVE STATS
 
@@ -304,16 +326,16 @@ def beautify_data_comp(field_name: str, dependency_field_name: str,
 
     return subset_data
 
-def evaluate_comparative_dependency_field(field_name: str, data: pd.DataFrame, strategy):
+def evaluate_comparative_dependency_field(field_name: str, dataFrame: DataFrame, strategy):
     """
     Perform a statistical analysis strategy for each 
     dependency field of a given classification field.
     Act as a wrapper for the comparative statistical
     functions
     """
-    field_names = list(data.columns)
+    field_names = list(dataFrame.data.columns)
 
-    return {dependency_field_name: strategy(field_name, dependency_field_name, data)
+    return {dataFrame.variable_type[dependency_field_name]: strategy(field_name, dependency_field_name, dataFrame.data)
              for dependency_field_name in field_names if dependency_field_name != field_name}
 
 ## Frequency Tables
@@ -325,8 +347,8 @@ def generate_comparative_violin_plot(field_name: str, dependency_field_name: str
     return beautify_data_comp(field_name, dependency_field_name,
                                       variable, dependency_variable, data)
 
-violin_plot_vector = {NominalVariables[field_name]: evaluate_comparative_dependency_field(field_name, nominal_df, generate_comparative_violin_plot)
-                       for field_name in nominal_df.columns}
+comp_distr_vector = {NominalVariables[field_name]: evaluate_comparative_dependency_field(field_name, nominal, generate_comparative_violin_plot)
+                       for field_name in nominal.data.columns}
 
 ## Bar Plots
 
@@ -339,17 +361,30 @@ def generate_stacked_bar_plot(field_name: str, dependency_field_name: str, data:
 
     if subset_data.empty: return
 
-    plt.figure(figsize=(10, 6))
-    sns.barplot(x=field_name, y='Frequency', hue=dependency_field_name, data=subset_data)
+    # Pivot the data to get a matrix form
+    pivoted_data = subset_data.pivot(index=field_name, columns=dependency_field_name, values='Frequency')
+
+    # Replace NaN values with 0
+    pivoted_data = pivoted_data.fillna(0)
+
+    fig, ax = plt.subplots(figsize=(10, 6))
+
+    # Bottom value for stacking
+    bottom_value = pd.Series([0] * pivoted_data.shape[0], index=pivoted_data.index)
+
+    for col in pivoted_data.columns:
+        plt.bar(pivoted_data.index, pivoted_data[col], bottom=bottom_value, label=col)
+        bottom_value += pivoted_data[col]
 
     plt.title(f"{variable.title} and {dependency_variable.title} ~ Stacked bar plot")
-    plt.gca().set_xlabel('')
+    plt.xlabel(variable.title)
     plt.ylabel('Frequency')
+    plt.legend(title=dependency_field_name)
 
-    return plt
+    return fig
 
-stacked_bar_plot_vector = {NominalVariables[field_name]: evaluate_comparative_dependency_field(field_name, nominal_df, generate_stacked_bar_plot)
-                       for field_name in nominal_df.columns}
+stacked_bar_plot_vector = {NominalVariables[field_name]: evaluate_comparative_dependency_field(field_name, nominal, generate_stacked_bar_plot)
+                       for field_name in nominal.data.columns}
 
 ## Grouped Bar Plots
 
@@ -362,17 +397,17 @@ def generate_grouped_bar_plot(field_name: str, dependency_field_name: str, data:
 
     if subset_data.empty: return
 
-    plt.figure(figsize=(10, 6))
+    fig, ax = plt.subplots(figsize=(10, 6))
     sns.barplot(x=field_name, y='Frequency', hue=dependency_field_name, data=subset_data, dodge=True)
 
     plt.title(f"{variable.title} and {dependency_variable.title} ~ Grouped bar plot")
     plt.gca().set_xlabel('')
     plt.ylabel('Frequency')
 
-    return plt
+    return fig
 
-grouped_bar_plot_vector = {NominalVariables[field_name]: evaluate_comparative_dependency_field(field_name, nominal_df, generate_grouped_bar_plot)
-                       for field_name in nominal_df.columns}
+grouped_bar_plot_vector = {NominalVariables[field_name]: evaluate_comparative_dependency_field(field_name, nominal, generate_grouped_bar_plot)
+                       for field_name in nominal.data.columns}
 
 ## Bubble Charts
 
@@ -386,7 +421,7 @@ def generate_bubble_chart(field_name: str, dependency_field_name: str, data: pd.
     if subset_data.empty: return
 
     # Creating the bubble chart
-    plt.figure(figsize=(10, 6))
+    fig, ax = plt.subplots(figsize=(10, 6))
     sns.scatterplot(data=subset_data, x=field_name, y=dependency_field_name, size='Frequency', color='black')
 
     # Adding labels and title
@@ -394,10 +429,10 @@ def generate_bubble_chart(field_name: str, dependency_field_name: str, data: pd.
     plt.gca().set_xlabel('')
     plt.gca().set_ylabel('')
 
-    return plt
+    return fig
 
-bubble_chart_vector = {NominalVariables[field_name]: evaluate_comparative_dependency_field(field_name, nominal_df, generate_bubble_chart)
-                       for field_name in nominal_df.columns}
+bubble_chart_vector = {NominalVariables[field_name]: evaluate_comparative_dependency_field(field_name, nominal, generate_bubble_chart)
+                       for field_name in nominal.data.columns}
 
 ## Fisher's Exact Test
 
@@ -424,8 +459,8 @@ def fisher_exact_test(field_name: str, dependency_field_name: str, data: pd.Data
     # return fisher_result
     return fisher_result
 
-fisher_exact_test_vector = {NominalVariables[field_name]: evaluate_comparative_dependency_field(field_name, nominal_df, fisher_exact_test)
-                       for field_name in nominal_df.columns}
+fisher_exact_test_vector = {NominalVariables[field_name]: evaluate_comparative_dependency_field(field_name, nominal, fisher_exact_test)
+                       for field_name in nominal.data.columns}
 
 ## Shapiro Wilk's Correlation Test
 
@@ -436,8 +471,8 @@ def shapiro_wilk_test(field_name: str, continuous_df: pd.DataFrame):
 
     return shapiro_result
 
-shapiro_wilk_test_vector = {ContinuousVariables[field_name]: shapiro_wilk_test(field_name, continuous_df)
-                          for field_name in continuous_df.columns}
+shapiro_wilk_test_vector = {ContinuousVariables[field_name]: shapiro_wilk_test(field_name, continuous.data)
+                          for field_name in continuous.data.columns}
 
 ## Pearson's Correlation Test
 
@@ -452,8 +487,8 @@ def pearson_cor_test(field_name: str, dependency_field_name: str, data: pd.DataF
 
     return pearson_coefficient, p_value
 
-pearson_cor_test_vector = {ContinuousVariables[field_name]: evaluate_comparative_dependency_field(field_name, continuous_df, pearson_cor_test)
-                       for field_name in continuous_df.columns}
+pearson_cor_test_vector = {ContinuousVariables[field_name]: evaluate_comparative_dependency_field(field_name, continuous, pearson_cor_test)
+                       for field_name in continuous.data.columns}
 
 ## Spearman's Correlation Test
 
@@ -468,5 +503,5 @@ def spearman_cor_test(field_name: str, dependency_field_name: str, data: pd.Data
 
     return spearman_result
 
-spearman_cor_test_vector = {ContinuousVariables[field_name]: evaluate_comparative_dependency_field(field_name, continuous_df, spearman_cor_test)
-                       for field_name in continuous_df.columns}
+spearman_cor_test_vector = {ContinuousVariables[field_name]: evaluate_comparative_dependency_field(field_name, continuous, spearman_cor_test)
+                       for field_name in continuous.data.columns}
